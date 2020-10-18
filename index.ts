e2eca79b6f0ef5016057f2926ba78b2de449c622
thetayloredman <nutdriver716@gmail.com>
2020-10-18 15:07:43 -0700

    Initialize old repo

---
+++ index.ts
@@ -0,0 +1,13 @@
+// mathengine
+// By BadBoyHaloCat
+
+// Modules
+
+// Utils
+import * as utils from './utils/index';
+import * as inquirer from 'inquirer';
+
+// Main
+(async function() {
+    await utils.readFile('./mathengine/')
+})();
---
+++ utils/index.ts
@@ -0,0 +1,8 @@
+// mathengine
+// By BadBoyHaloCat
+
+// Index file
+export * from './readFile';
+export * from './writeFile';
+export * from './appendFile';
+export * from './readdir';
